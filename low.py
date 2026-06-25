aee5c02fb1b70f79fc2f353f868a9b12e3cd06d9
Frank <globaltzzy@gmail.com>
2026-06-26 01:36:44 +0200

    clean signal and add FIR filter function
    Modified the clean audio signal to include two sine waves and added a FIR filter function to process the noisy signal.

--- low.py
+++ low.py
@@ -13,7 +13,7 @@ duration = 3.0
 t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
 
 # we generate a clean audio signal
-clean_signal = np.sin(2 * np.pi * 400 * t)
+clean_signal = np.sin(2 * np.pi * 200 * t) + np.sin(2 * np.pi * 300 * t)
 
 # in order to hear the difference we create noise
 noise = 0.5 * np.sin(2 * np.pi * 4000 * t)
@@ -28,4 +28,36 @@ def save_wav(filename, audio_data, rate):
     wavfile.write(filename, rate, normalized)
     print(f"Saved: {filename}")
 
-    save_wav("1_noisy_input.wav", noisy_signal, sample_rate)
+
+save_wav("1_noisy_input.wav", noisy_signal, sample_rate)
+
+
+def fir_signal(x, b_coeffs):
+
+    N = len(b_coeffs)
+
+    total_samples = len(x)
+
+    y = np.zeros(total_samples)
+
+    for n in range(total_samples):
+        sample_sum = 0.0
+
+        for k in range(N):
+            if n - k >= 0:
+                sample_sum += b_coeffs[k] * x[n - k]
+
+        y[n] = sample_sum
+
+    return y
+
+
+num_taps = 200
+b_moving_average = [1.0 / num_taps] * num_taps
+
+print("Filtering noise (this may take a few seconds in pure Python)...")
+
+y_fir = fir_signal(noisy_signal, b_moving_average)
+
+
+save_wav("4_filtered_noise_FIR.wav", y_fir, sample_rate)
